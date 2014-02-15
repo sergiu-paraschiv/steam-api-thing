cc065034f385dd1a179516ce743d1996f2c23078
sergiu-paraschiv <sergiuspk@gmail.com>
2014-02-15 13:36:43 +0200

    SteamAPI config file

---
+++ config/SteamAPI.js
@@ -0,0 +1,13 @@
+(function(undefined) {
+    'use strict';
+    
+    this.Config = {
+        SteamAPI: {
+            url: 'http://api.steampowered.com/',
+            version: '',
+            key: '8F103D67D6C67745E17807F40205ADA0',
+            format: 'json'
+        }
+    };
+    
+}).call(this);
